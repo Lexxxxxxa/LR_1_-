1b914ce6e6af1a6e4b78cd440fe2ba0257aeef23
Oleksiy Zubtsov <zubcov.alexv@gmail.com>
2023-02-26 21:30:13 +0200

    Добавьте файлы проекта.

---
+++ LR_1_ХмОбч/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LR_1_ХмОбч.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
